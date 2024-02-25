58bbb090b1bfdfa400bd420741d79ec18d9f3ee8
naol86 <lijnaolkasinet@gmail.com>
2024-02-25 10:40:12 +0300

    test

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''returns information about TODO list progress by employee id.'''
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    data = requests.get('https://jsonplaceholder.typicode.com/todos')
+    data = data.json()
+    users = requests.get('https://jsonplaceholder.typicode.com/users')
+    users = users.json()
+    user = [i for i in users if i.get('id') == int(argv[1])][0]
+    tasks = [i for i in data if i.get('userId') == int(argv[1])]
+    completed = [i for i in tasks if i.get('completed') is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get('name'), len(completed), len(tasks)))
+    for task in completed:
+        print("\t {}".format(task.get('title')))

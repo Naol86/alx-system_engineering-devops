72a1fd57a584df950c95358c5c1cc43da1c1b71f
naol86 <naol.kasinet@a2sv.org>
2024-03-14 12:50:53 +0300

    test

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,9 @@
-# Increases the amount of traffic an Nginx server can handle.
-# Increase the ULIMIT of the default file
-
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 } ->
 
-# Restart Nginx
 exec { 'nginx-restart':
-  command => 'nginx restart',
+  command => 'service nginx restart',
   path    => '/etc/init.d/'
 }

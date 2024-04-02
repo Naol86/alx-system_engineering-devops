845692f25f6f29f6c9cc125a9fc17494103837f6
naol86 <naol.kasinet@a2sv.org>
2024-04-02 09:14:51 +0300

    test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """queries the Reddit API"""
     headers = {
-        "User-Agent": "0x16. API_advanced-e_kiminza"
+        "User-Agent": "0x16. API_advanced-e_kiminza using python-requests"
         }
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
